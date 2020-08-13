cd7d8dff7c923623a3065fba59d9e77000f4417a
Dakota Coppage <dakotakcoppage@me.com>
2020-08-13 17:03:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,7 +31,9 @@ class Song
     genres = {}
     @@genres.each do |genre|
       if genre_count[genre]
-        genre_count =+ 1 
+        genre_count[genre] =+ 1
+      else
+        genre_count[genre] = 0 
   
     
   
