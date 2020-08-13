56ce859170246868586f698ad5ede93847dffdfe
Dakota Coppage <dakotakcoppage@me.com>
2020-08-13 17:12:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -53,7 +53,7 @@ class Song
 
 end
 
-lucifer
-ninety_nine_problems = Song.new("Lucifer", "Jay-Z", "rap" )
-hit_me = Song.new("Brittany Spears", )
+lucifer = Song.new()
+ninety_nine_problems = Song.new("99 Problems", "Jay-Z", "rap" )
+hit_me = Song.new("hit me baby one more time", "Brittany Spears", "pop" )
  
